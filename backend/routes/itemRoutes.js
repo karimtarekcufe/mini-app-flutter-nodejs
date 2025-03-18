c6914a1f18ee650b386d99b0feb34bdae47f56aa
Karim Tarek Abdalla Abdelhalim <148661123+karimtarekcufe@users.noreply.github.com>
2025-03-18 19:16:58 +0200

    ho

--- backend/routes/itemRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getItems, getItemById } = require('../controllers/itemController');
-const { protect } = require('../middleware/auth');
-
-router.get('/', getItems);
-router.get('/:id', getItemById);
-
-module.exports = router;
--- frontend/lib/constants.dart
+++
@@ -1,2 +0,0 @@
-const String baseUrl =
-    "http://10.0.2.2:5000"; // Replace PORT with your actual Node.js port
